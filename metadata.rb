1b5d133fc6dd4d9416f2be1843bc5f1c3fcb8720
Chris Williams <chris.a.williams@gmail.com>
2019-11-14 12:46:18 -0500

    chore(release): bump version to 2.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Christopher Williams'
 maintainer_email 'cwilliams@axway.com'
 license 'Apache-2.0'
 description 'Manages Google Android SDK/NDK/emulators'
-version '2.1.0'
+version '2.1.1'
 issues_url 'https://github.com/appcelerator/chef-android-sdk/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/appcelerator/chef-android-sdk/' if respond_to?(:source_url)
 chef_version '>= 12.5' if respond_to?(:chef_version)
